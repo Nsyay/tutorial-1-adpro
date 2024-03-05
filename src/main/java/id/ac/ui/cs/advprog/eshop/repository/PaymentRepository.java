2b73e35952b749cace590f3e494c0f61213a7838
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-03-05 23:21:10 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,10 +10,48 @@ import java.util.Map;
 public class PaymentRepository{
     private List<Payment> payments = new ArrayList<>();
 
-    public Payment save(Payment payment){ return null; }
-    public Payment getPayment(String id){return null;}
-    public List<Payment> findVoucherPayment(){return null;}
-    public List<Payment> findBankTransferPayment(){return null;}
-    public List<Payment> getAllPayments(){return null;}
+    public Payment save(Payment payment){
+        int i=0;
+        for(Payment savedPayment : payments){
+            if(savedPayment.getId().equals(payment.getId())){
+                payments.remove(i);
+                payments.add(i, payment);
+                return payment;
+            }
+            i+=1;
+        }
+        payments.add(payment);
+        return payment;
+    }
+    public Payment getPayment(String id){
+        for(Payment savedPayment : payments){
+            if(savedPayment.getId().equals(id)){
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+    public List<Payment> findVoucherPayment(){
+        List<Payment> voucherPayments = new ArrayList<>();
+        for(Payment savedPayment : payments){
+            System.out.println(savedPayment.getMethod());
+            if(savedPayment.getMethod().equals("VOUCHER_CODE")){
+                voucherPayments.add(savedPayment);
+            }
+        }
+        return voucherPayments;
+    }
+    public List<Payment> findBankTransferPayment(){
+        List<Payment> bankTransferPayments = new ArrayList<>();
+        for(Payment savedPayment : payments){
+            if(savedPayment.getMethod().equals("BANK_TRANSFER")){
+                bankTransferPayments.add(savedPayment);
+            }
+        }
+        return bankTransferPayments;
+    }
+    public List<Payment> getAllPayments(){
+        return payments;
+    }
 
 }
